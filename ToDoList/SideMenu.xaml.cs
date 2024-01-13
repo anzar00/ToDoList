a05af1bd4edc5a89b38acc8f35c31a9531bfd23e
anzar00 <peerzada.anzar@gmail.com>
2024-01-13 20:36:17 +0530

    Basic Side Menu added

---
+++ ToDoList/SideMenu.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToDoList
+{
+    /// <summary>
+    /// Interaction logic for SideMenu.xaml
+    /// </summary>
+    public partial class SideMenu : UserControl
+    {
+        public SideMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void NavigateToHomePage(object sender, RoutedEventArgs e)
+        {
+            // To be implemented later
+        }
+
+        private void NavigateToUpcomingPage(object sender, RoutedEventArgs e)
+        {
+            // To be implemented later
+        }
+
+        private void NavigateToTodayPage(object sender, RoutedEventArgs e)
+        {
+            // To be implemented later
+        }
+
+        private void NavigateToCalendarPage(object sender, RoutedEventArgs e)
+        {
+            // To be implemented later
+        }
+
+        private void NavigateToSettings(object sender, RoutedEventArgs e)
+        {
+            // To be implemented later
+        }
+
+        private void NavigateToListWork(object sender, RoutedEventArgs e)
+        {
+            // To be implemented later
+        }
+
+        private void NavigateToListPersonal(object sender, RoutedEventArgs e)
+        {
+            // To be implemented later
+        }
+
+        private void NavigateToListStudy(object sender, RoutedEventArgs e)
+        {
+            // To be implemented later
+        }
+
+        private void SignOut(object sender, RoutedEventArgs e)
+        {
+            // To be implemented later
+        }
+
+        private void NavigateToAddNewList(object sender, RoutedEventArgs e)
+        {
+            // To be implemented later
+        }
+    }
+}
